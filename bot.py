81d66966328fdc703398db7bc6f3ac8e2b8bd2cf
astanahunters <astanahunters@gmail.com>
2025-06-13 02:33:36 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ GOOGLE_CREDENTIALS = os.environ.get("GOOGLE_CREDENTIALS", "/etc/secrets/GOOGLE_C
 print(f"GOOGLE_CREDENTIALS = {GOOGLE_CREDENTIALS}")
 SPREADSHEET_NAME = 'astanahunters_template'
 PRIVATE_CHAT_ID = -1002635314764 # Заменить на свой chat_id закрытого чата
-INVITE_LINK = "Теперь вход платный! Пишите админу ЛС"
+INVITE_LINK = "Теперь вход платный! Пишите админу ЛС @astanahunters"
 YOUR_ADMIN_ID = 7796929428
 
 # --- Google Sheets подключение ---
